2df9597ca6ed285cbd0c76f9c139e029d0149a93
AceofSpades5757 <10341888+AceofSpades5757@users.noreply.github.com>
2022-04-30 02:10:03 -0500

    Add plugin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+/// Simple Vim plugin written in Rust and implemented with rust-plug
+use std::io::Write;
+use std::net::TcpListener;
+use std::net::TcpStream;
+
+use simple_logger::SimpleLogger;
+
 fn main() {
-    println!("Hello, world!");
+    SimpleLogger::new().init().unwrap();
+
+    // Config
+    let ip = "127.0.0.1";
+    let port = "8765";
+
+    let listener = TcpListener::bind(format!("{ip}:{port}")).unwrap();
+
+    match listener.accept() {
+        Ok((mut stream, addr)) => {
+            log::info!("New Client: {:?}", addr);
+            log::info!("New Client: {:?}", stream);
+            log::info!("Starting Plugin.");
+            plugin(&stream);
+            log::info!("Plugin Run Successfully.");
+        }
+        Err(err) => log::warn!("Unable to get client: {:?}", err),
+    }
+}
+
+/// Vim Plugin
+///
+/// Set a global variable
+fn plugin(mut stream: &TcpStream) {
+    use vii::channel::ChannelCommand;
+    use vii::channel::ExCommand;
+
+    // Vars
+    let variable = "rust_plug_plugin_poc";
+    let value = "Hello Vim!".to_string();
+
+    // Set Global Variable
+    let command: String = format!(r#"let g:{variable} = '{value}'"#);
+    let ex = ChannelCommand::Ex(ExCommand { command });
+    let channel_command = ex.to_string();
+
+    log::info!("Sending Command: {:?}", channel_command);
+    stream.write(channel_command.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
